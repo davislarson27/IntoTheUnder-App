f0be012b1ba25892185b1d5ce413b9ced65e686a
Davis Larson <davislarson27@outlook.com>
2026-05-05 11:46:21 -0700

    set up new icon

--- play/bg_mining_icon.py
+++ play/bg_mining_icon.py
@@ -5,61 +5,51 @@ from .bg_overlay import BG_Overlay
 
 class Bg_Mining_Icon:
     def __init__(self, screen, x, y, icon_width, icon_height):
+        # region set attributes
         self.screen = screen
         self.x = x
         self.y = y
 
-        block_width = icon_width//3
+        block_width = (icon_width * 2) // 3
+        self.foreground_block_offset_position = icon_width - block_width
+        # endregion
 
-        # alpha levels
-        base_alpha = 235
-        active_alpha = 230
-        inactive_alpha = 85
+        # region set surfaces
+        self.foreground_icon = pygame.Surface((block_width, block_width), pygame.SRCALPHA).convert_alpha()
+        self.background_icon_active = pygame.Surface((block_width, block_width), pygame.SRCALPHA).convert_alpha()
+        self.background_icon_inactive = pygame.Surface((block_width, block_width), pygame.SRCALPHA).convert_alpha()
+        # endregion
 
-        # draw the active icon
-        self.active_icon_surface = pygame.Surface((icon_width, icon_height), pygame.SRCALPHA).convert_alpha()
-        Grass.draw_manual(self.active_icon_surface, 0, 0, block_width, is_grid_coordinates=False)
-        Dirt.draw_manual(self.active_icon_surface, 0, block_width, block_width, is_grid_coordinates=False)
-        Grass.draw_manual(self.active_icon_surface, block_width, 0, block_width, is_grid_coordinates=False)
-        Grass.draw_manual(self.active_icon_surface, block_width, block_width, block_width, is_grid_coordinates=False)
-        self.active_icon_surface.set_alpha(active_alpha)
+        # region set blocks
+        Grass.draw_manual(self.background_icon_active, 0, 0, block_width, is_grid_coordinates=False)
+        Grass.draw_manual(self.background_icon_inactive, 0, 0, block_width, is_grid_coordinates=False)
+        Grass.draw_manual(self.foreground_icon, 0, 0, block_width, is_grid_coordinates=False)
+        # endregion
 
-        # draw the inactive icon
-        self.inactive_icon_surface = pygame.Surface((icon_width, icon_height), pygame.SRCALPHA).convert_alpha()
-        Grass.draw_manual(self.inactive_icon_surface, 0, 0, block_width, is_grid_coordinates=False) # (0, 0)
-        Dirt.draw_manual(self.inactive_icon_surface, 0, block_width, block_width, is_grid_coordinates=False) # (1, 0)
-        Grass.draw_manual(self.inactive_icon_surface, block_width, 0, block_width, is_grid_coordinates=False) # (0, 1)
-        self.inactive_icon_surface.set_alpha(inactive_alpha)
-
-        self.base_icon_surface = pygame.Surface((icon_width, icon_height), pygame.SRCALPHA).convert_alpha()
-        Grass.draw_manual(self.base_icon_surface, block_width, block_width, block_width, is_grid_coordinates=False) # (1, 1)
-        Grass.draw_manual(self.base_icon_surface, block_width*2, block_width, block_width, is_grid_coordinates=False) # (2, 1)
-        Dirt.draw_manual(self.base_icon_surface, block_width*2, block_width*2, block_width, is_grid_coordinates=False) # (2, 2)
-        Dirt.draw_manual(self.base_icon_surface, block_width, block_width*2, block_width, is_grid_coordinates=False) # (1, 2)
-        # self.base_icon_surface.set_alpha(active_alpha)
-
-        bg_overlay = BG_Overlay(screen, block_width, None, None)
-        self.inactive_icon_surface.set_alpha(inactive_alpha)
-
-        exposures = [
-            # (0,         0,             False,  True, False,  True),  # top-left:     (x, y, top, bottom, left, right)
-
-            (0,         0,             False,  False, False,  False),  # top-left:     (x, y, top, bottom, left, right)
-            (block_width,         0,   False,  True, False,  False),  #               (x, y, top, bottom, left, right)
-            (0,         block_width,   False,  False, False,  True),  #               (x, y, top, bottom, left, right)
-
-            # (block_width, block_width, True,  False, True, False), # bottom-right
+        # region bg overlays
+        exposures = [ # not drawing correctly at the moment
+            (0, 0, False, True, False, True),  # top-left corner of bg: shadow on bottom + right (where fg overlaps)
         ]
+        bg_overlay = BG_Overlay(screen, block_width, None, None)
 
         for draw_x, draw_y, top, bottom, left, right in exposures:
             surf = bg_overlay.get_bg_overlay_surface(top, bottom, left, right)
-            self.active_icon_surface.blit(surf, (draw_x, draw_y))
-            self.inactive_icon_surface.blit(surf, (draw_x, draw_y))
+            self.background_icon_active.blit(surf, (draw_x, draw_y))
+            self.background_icon_inactive.blit(surf, (draw_x, draw_y))
+        # endregion
 
+        # region set alpha
+        active_alpha = 230
+        inactive_alpha = 85
+
+        self.background_icon_active.set_alpha(active_alpha)
+        self.background_icon_inactive.set_alpha(inactive_alpha)
+        # endregion
 
     def draw(self, input):
         if input.caps_lock:
-            self.screen.blit(self.active_icon_surface, (self.x, self.y))
+            self.screen.blit(self.background_icon_active, (self.x, self.y))
         else:
-            self.screen.blit(self.inactive_icon_surface, (self.x, self.y))
-        self.screen.blit(self.base_icon_surface, (self.x, self.y))
+            self.screen.blit(self.background_icon_inactive, (self.x, self.y))
+        
+        self.screen.blit(self.foreground_icon, (self.x+self.foreground_block_offset_position, self.y+self.foreground_block_offset_position))
