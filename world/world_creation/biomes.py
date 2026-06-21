ead1532f83ffd87d48fde24866b4c0b80ecd3c99
Davis Larson <davislarson27@outlook.com>
2026-06-21 14:07:32 -0700

    removed mohagany tree from some biomes

--- world/world_creation/biomes.py
+++ world/world_creation/biomes.py
@@ -201,7 +201,6 @@ class Spruce_Forest(Biome):
         Structure_Identifier(Spruce_Tree, 0.1),
         Structure_Identifier(Small_Bush, 0.01),
         Structure_Identifier(Flowers, 0.028),
-        Structure_Identifier(Mahagony_Tree_Rand_Width, 0.3),
     ]
     bg_structures = [ # make sure that odds combined do not add up even close to 100 or the whole area will be covered
         Structure_Identifier(Spruce_Tree, 0.035)
@@ -222,7 +221,7 @@ class Forest(Biome):
         Structure_Identifier(Tree, 0.1),
         Structure_Identifier(Small_Bush, 0.01),
         Structure_Identifier(Flowers, 0.028),
-        Structure_Identifier(Mahagony_Tree_Rand_Width, 0.3),
+        Structure_Identifier(Mahagony_Tree_Rand_Width, 0.001),
     ]
     bg_structures = [ # make sure that odds combined do not add up even close to 100 or the whole area will be covered
         Structure_Identifier(Tree, 0.1)
@@ -246,7 +245,6 @@ class Montane_Forest(Biome): # at some point get more gravel to appear here near
         Structure_Identifier(Flowers, 0.028),
         Structure_Identifier(Puddle, 0.012),
         Structure_Identifier(Spruce_Tree, 0.0003),
-        Structure_Identifier(Mahagony_Tree_Rand_Width, 0.3),
     ]
     bg_structures = [ # make sure that odds combined do not add up even close to 100 or the whole area will be covered
         Structure_Identifier(Tree, 0.019)
